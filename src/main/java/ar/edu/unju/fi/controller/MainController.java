5283448a2538cbcad6711d37880cc8425ebce2ea
fertejj <fertejj@fertejj-VivoBook-ASUSLaptop-X415EA-F415EA>
2024-06-06 16:46:36 -0300

    cree footer y header fragment y los importe al index

--- src/main/java/ar/edu/unju/fi/controller/MainController.java
+++ src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -1,5 +1,14 @@
 package ar.edu.unju.fi.controller;
 
-public class MainController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/crud")
+public class MainController {
+	@GetMapping("/principal")
+	public String getIndexPage() {
+		return "index";
+	}
 }
